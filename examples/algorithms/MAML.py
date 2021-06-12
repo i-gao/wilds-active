274b425b40f7f2fb0f2475394f9d8e3763388ef0
i-gao <igao@stanford.edu>
2021-06-11 23:13:19 -0700

    remove debugging logging

--- examples/algorithms/MAML.py
+++ examples/algorithms/MAML.py
@@ -2,7 +2,6 @@ import torch
 from algorithms.single_model_algorithm import SingleModelAlgorithm
 from models.initializer import initialize_model
 import learn2learn as l2l
-import torch.autograd.profiler as profiler
 
 class MAML(SingleModelAlgorithm):
     def __init__(self, config, d_out, grouper, loss,
@@ -28,9 +27,6 @@ class MAML(SingleModelAlgorithm):
         )
         self.meta_model = meta_model
         del self.model
-
-        print('INIT') 
-        profile_cuda_memory()
     
     def objective(self, results):
         return self.loss.compute(results['y_pred'], results['y_true'], return_dict=False)
@@ -38,40 +34,20 @@ class MAML(SingleModelAlgorithm):
     def adapt_task(self, adapt_batch, eval_loader):
         """Clones meta_model -> task_model and trains task_model on train_data"""
         self.optimizer.zero_grad()
-
-        print('START OF ADAPT_TASK') 
-        profile_cuda_memory()
-
-        # move meta_model to cpu, move model to cuda
         self.model = self.meta_model.clone()
-        # self.model.to(self.device)
         torch.cuda.empty_cache()
-        print(f">>>>>>>> Task model is on CUDA: {check_model_device(self.model, desire_cuda=True)}")
-        print(f">>>>>>>> Meta model is on CUDA: {check_model_device(self.meta_model, desire_cuda=True)}")
-       
-      
-        self._train_task_model(adapt_batch)
- 
+
+        self._train_task_model(adapt_batch) 
         results = self._eval_task_model(eval_loader, call_backward=True)
 
-
-        # move meta_model to cuda, move model to cpu
         self.model.to('cpu')
-        # self.meta_model.to(self.device)
-        print(f">>>>>>>> Task model is on CPU: {check_model_device(self.model, desire_cuda=False)}")
-        print(f">>>>>>>> Meta model is on CUDA: {check_model_device(self.meta_model, desire_cuda=True)}")
-        
         self.optimizer.step()
         self.step_schedulers(
             is_epoch=False,
             metrics=results,
             log_access=False)
-
         del self.model
         torch.cuda.empty_cache()
-
-        print('END OF ADAPT_TASK') 
-        profile_cuda_memory()
         
     def evaluate(self, adapt_data, eval_loader):
         self.optimizer.zero_grad()
@@ -89,10 +65,7 @@ class MAML(SingleModelAlgorithm):
             results = self.process_batch(task_adaptation_data)
             train_error = self.objective(results)
             self.model.adapt(train_error)
-            train_error.detach()
-        
-        print('AFTER TRAIN TASK') 
-        profile_cuda_memory()      
+            train_error.detach() 
                 
     def _eval_task_model(self, task_eval_loader, call_backward=False):
         self.model.eval()
@@ -105,16 +78,9 @@ class MAML(SingleModelAlgorithm):
 
         with torch.set_grad_enabled(call_backward):
             for batch in task_eval_loader:
-                print('BATCH') 
-                profile_cuda_memory()
-
                 batch_results = self.process_batch(batch)
                 batch_objective = self.objective(batch_results)
-
-                if call_backward: batch_objective.backward()  # do I retain graph here or not?
-
-                print(">>>>>>>>>>>>> Meta model gradient norms: \n")
-                print_model(self.meta_model)
+                if call_backward: batch_objective.backward()
 
                 epoch_y_true.append(batch_results['y_true'].detach().clone())
                 epoch_g.append(batch_results['g'].detach().clone())
@@ -127,23 +93,3 @@ class MAML(SingleModelAlgorithm):
             'metadata': torch.cat(epoch_metadata),
             'y_pred': torch.cat(epoch_y_pred)
         }
-
-
-def profile_cuda_memory():
-    t = torch.cuda.get_device_properties(0).total_memory
-    r = torch.cuda.memory_reserved(0) 
-    a = torch.cuda.memory_allocated(0)
-    print(f'>>>> CUDA Memory Status: total: {t}, allocated/needed: {a}, cached: {r-a}')
-
-def print_model(model):
-    out = {}
-    for name, param in model.named_parameters():
-        if param.grad is not None: out[name] = param.grad.norm().data
-        else: out[name] = None
-    print(out)
-
-def check_model_device(model, desire_cuda=False):
-    """make sure all params on this model are the desired device"""
-    for param in model.parameters():
-        if param.is_cuda != desire_cuda: return False
-    return True
