bd4f2cb517591477e7a88f16807c5d1400ec039a
i-gao <igao@stanford.edu>
2021-08-02 22:11:27 -0700

    ensure fixmatch has classifier and featurizer lrs defined

--- examples/algorithms/fixmatch.py
+++ examples/algorithms/fixmatch.py
@@ -34,11 +34,12 @@ class FixMatch(SingleModelAlgorithm):
         )
         model = torch.nn.Sequential(featurizer, classifier)
 
-        parameters_to_optimize: List[Dict] = [
-            {"params": featurizer.parameters(), "lr": config.fixmatch_featurizer_lr},
-            {"params": classifier.parameters(), "lr": config.fixmatch_classifier_lr},
-        ]
-        self.optimizer = initialize_optimizer_with_model_params(config, parameters_to_optimize)
+        if config.fixmatch_featurizer_lr and config.fixmatch_classifier_lr:
+            parameters_to_optimize: List[Dict] = [
+                {"params": featurizer.parameters(), "lr": config.fixmatch_featurizer_lr},
+                {"params": classifier.parameters(), "lr": config.fixmatch_classifier_lr},
+            ]
+            self.optimizer = initialize_optimizer_with_model_params(config, parameters_to_optimize)
 
         # initialize module
         super().__init__(
