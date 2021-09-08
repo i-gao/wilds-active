6a974718a21d6206815966e18f4a27f81a97ec32
i-gao <igao@stanford.edu>
2021-09-07 19:55:08 -0700

    update run scripts to allow turning off warm starting

--- run-scripts/run-batch.sh
+++ run-scripts/run-batch.sh
@@ -10,6 +10,7 @@ tau=0.8
 seed=0
 savepred=1
 stepevery=2
+pretrained=True
 
 #######
 
@@ -38,10 +39,14 @@ function run_expt() {
         _savepred="--save_pred_step ${savepred}"
     fi
 
+    erm_path="/juice/scr/scr110/scr/nlp/wilds/baselines/${dataset}/official_erm/log/${dataset}_seed:0_epoch:best_model.pth"
+
     if [ $algorithm == "NoisyStudent" ]; then
-        _model_flag="teacher_model_path"
+        _model_flag="--teacher_model_path ${erm_path}"
+    elif [ $pretrained == "True" ]; then
+        _model_flag="--pretrained_model_path ${erm_path}"
     else
-        _model_flag="pretrained_model_path"
+        _model_flag=""
     fi
 
     if [ $labels == "tgt" ]; then
@@ -68,7 +73,7 @@ function run_expt() {
 
     #######
 
-    cmd="CUDA_VISIBLE_DEVICES=$(seq -s, $counter 0 $(($gpus - 1))) python /juice/scr/irena/wilds-active/examples/run_expt.py --root_dir /u/scr/nlp/dro --device $(seq 0 $(($gpus - 1))) --loader_kwargs num_workers=$((4 * $gpus)) --log_dir $dir --dataset $dataset --lr $lr --weight_decay $wd --batch_size $labeled --unlabeled_batch_size $unlabeled --algorithm $algorithm --${_model_flag} /juice/scr/scr110/scr/nlp/wilds/baselines/${dataset}/official_erm/log/${dataset}_seed:0_epoch:best_model.pth --n_epochs $epochs --active_learning --selection_function $sf --n_shots $K $_selectby --eval_splits unlabeled_test id_val id_test --self_training_unlabeled_weight $unlabeled_weight --self_training_threshold $tau $_label_args --seed $seed $_savepred --gradient_accumulation_steps $stepevery --use_wandb --wandb_api_key_path /sailhome/irena/.ssh/wandb-api-key --wandb_kwargs entity=i-gao project=ssda group=batch"
+    cmd="CUDA_VISIBLE_DEVICES=$(seq -s, $counter 0 $(($gpus - 1))) python /juice/scr/irena/wilds-active/examples/run_expt.py --root_dir /u/scr/nlp/dro --device $(seq 0 $(($gpus - 1))) --loader_kwargs num_workers=$((4 * $gpus)) --log_dir $dir --dataset $dataset --lr $lr --weight_decay $wd --batch_size $labeled --unlabeled_batch_size $unlabeled --algorithm $algorithm $_model_flag --n_epochs $epochs --active_learning --selection_function $sf --n_shots $K $_selectby --eval_splits unlabeled_test id_val id_test --self_training_unlabeled_weight $unlabeled_weight --self_training_threshold $tau $_label_args --seed $seed $_savepred --gradient_accumulation_steps $stepevery --use_wandb --wandb_api_key_path /sailhome/irena/.ssh/wandb-api-key --wandb_kwargs entity=i-gao project=ssda group=batch"
 
     nlp_cmd="nlprun -o ${dir}/out -n active --anaconda-environment wilds-ig --queue jag --priority standard --gpu-type titanxp --gpu-count $gpus --memory 16g \"$cmd\""
 
